9745c28d4576f4ec7e0327423bd18d69473e89c3
master.yakiimo <yakiimo634@gmail.com>
2022-03-27 13:19:18 +0000

    Microposts.php

---
+++ app/Micropost.php
@@ -0,0 +1,14 @@
+<?php
+
+class Micropost extends Model
+{
+    protected $fillable = ['content'];
+
+    /**
+     * この投稿を所有するユーザ。（ Userモデルとの関係を定義）
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
